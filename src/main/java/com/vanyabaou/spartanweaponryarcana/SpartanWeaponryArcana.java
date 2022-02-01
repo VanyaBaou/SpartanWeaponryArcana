11a1c03877cb120404730d6f8f5cac695e6f15ad
VanyaBaou <97327658+VanyaBaou@users.noreply.github.com>
2022-02-01 12:59:43 -0800

    placeolders + ver fix
    now shows mod ver properly
    placeholders for parrying daggers

--- src/main/java/com/vanyabaou/spartanweaponryarcana/SpartanWeaponryArcana.java
+++ src/main/java/com/vanyabaou/spartanweaponryarcana/SpartanWeaponryArcana.java
@@ -23,7 +23,7 @@ public class SpartanWeaponryArcana {
 
     public static final String MOD_ID = "spartanweaponryarcana";
     public static final String MOD_NAME = "SpartanWeaponryArcana";
-    public static final String VERSION = "beta-1.4.0";
+    public static final String VERSION = "1.4.0";
     public static final String DEPENDENCIES = "required-after:spartanweaponry@[1.4.0,);after:baubles;after:botania;after:thaumcraft;after:embers";
     public static final String MCVERSION = "[1.12.2]";
     public static final String NETWORKCHANNEL = "swarcana";
